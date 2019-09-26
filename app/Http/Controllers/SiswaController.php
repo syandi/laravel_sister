d4d68ac42b05efa8e0535b13b31bd9b3dc2cedba
Syandi <MySQL@ksewu.local>
2019-09-26 15:55:08 +0700

    update

--- app/Http/Controllers/SiswaController.php
+++ app/Http/Controllers/SiswaController.php
@@ -6,5 +6,7 @@ use Illuminate\Http\Request;
 
 class SiswaController extends Controller
 {
-    //
+    public function index(){
+        return view('siswa.index');
+    }
 }
---
+++ resources/views/siswa/index.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+
+@section('content')
+    seumur hidup aku ini yang pertama
+@endsection
