af07be3660088951f11f348d1e255486d8fe2e0f
Syandi <MySQL@ksewu.local>
2019-10-18 16:33:50 +0700

    update

--- resources/views/siswa/create.blade.php
+++ resources/views/siswa/create.blade.php
@@ -1,5 +1,26 @@
 @extends('layouts.app')
 
 @section('content')
-halo
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-sm-6">
+            <form>
+                <div class="form-group">
+                    <label for="exampleInputEmail1">Email address</label>
+                    <input class="form-control" type="text" placeholder="">
+                    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                </div>
+                <div class="form-group">
+                    <label for="exampleInputPassword1">Password</label>
+                    <input class="form-control" type="text" placeholder="">
+                </div>
+                <div class="form-group form-check">
+                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+        </div>
+    </div>
+</div>
 @endsection
